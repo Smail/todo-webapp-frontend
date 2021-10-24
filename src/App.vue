990e26f8d60ed384f628c17787f8d16c9ea1c896
Smail <44433524+Smail@users.noreply.github.com>
2021-10-24 08:57:37 +0200

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
+  <!-- eslint-disable -->
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="sidebar">
+      <ProjectList :projects="defaultProjects" :active-project-id="activeProjectId"></ProjectList>
+      <hr v-if="userProjects.length > 0">
+      <ProjectList section-title="Projects" :projects="userProjects" :active-project-id="activeProjectId"></ProjectList>
+    </div>
+
+    <div id="middle-section">
+      <TasksView :project-id="activeProjectId"></TasksView>
+    </div>
+
+    <div id="right-section">
+
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Project from "@/components/Project";
+import ProjectList from "@/components/ProjectList";
+import TasksView from "@/components/TasksView";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TasksView,
+    ProjectList,
+    Project
+  },
+  data() {
+    return {
+      activeProjectId: 1,
+      defaultProjects: [
+        {id: 1, icon: 'inbox', text: 'Inbox'},
+        {id: 2, icon: 'calendar_today', text: 'Today'},
+        {id: 3, icon: 'upcoming', text: 'Upcoming'},
+      ],
+      userProjects: [],
+    }
   }
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  font-family: Roboto, sans-serif;
+}
+
+a, a:visited {
+  color: inherit;
+  text-decoration: none;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+#sidebar {
+  background: aqua;
+  flex: 2;
+}
+
+#middle-section {
+  background: red;
+  flex: 7;
+}
+
+#right-section {
+  background: green   ;
+  flex: 7;
 }
 </style>

925813b933a7ae449a9715fd57746b39a35a4470
Smail <44433524+Smail@users.noreply.github.com>
2021-10-25 03:19:04 +0200

    Add TasksView.vue

--- src/components/TasksView.vue
+++ src/components/TasksView.vue
@@ -1,33 +1,50 @@
 <template>
-  <h1></h1>
+  <section>
+    <h1>{{ project.name }}</h1>
+    <ul>
+      <li v-for="task in tasks">{{ task.name }}</li>
+    </ul>
+  </section>
 </template>
 
 <script>
 export default {
   name: "TasksView",
   props: {
-    projectId: Number,
+    project: {id: Number, icon: String, name: String},
+  },
+  data() {
+    return {
+      // [{id: String, name: String, content: String, duration: int, dueDate: String}]
+      tasks: [],
+    }
   },
   mounted() {
     // Request tasks from server after props are available with AJAX.
     // Props are for the first time available in the mounted hook.
     let vm = this;
-
-    const data = {
-      "action": "get_tasks",
-      "projectId": vm.projectId,
-    }
     // TODO change URL to HTTPS when SSL is activated on the server
     // Note: I changed my /etc/hosts file to redirect smail.de to localhost
-    $.post('http://api.todo.smail.de/', data, function (response) {
-      console.log($.parseJSON(response));
+
+    console.log(this.project);
+
+    $.post('http://api.todo.smail.de/ajax.php', {
+      "action": "get_tasks",
+      "projectId": this.project.id,
+    }, (response) => {
+      // TODO make this work
+      this.tasks = $.parseJSON(response);
+      console.log("jkhfdjhkdsdkl")
     }).fail(function (response) {
-      console.error(response.getHeaders());
+      console.log("err");
+      console.error(response);
     });
   }
 }
 </script>
 
 <style scoped>
-
+h1 {
+  text-transform: capitalize;
+}
 </style>

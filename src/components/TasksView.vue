a9ef8575c11ff3bfacd86a157bda8b631b2ad92a
Smail <44433524+Smail@users.noreply.github.com>
2021-11-01 21:26:26 +0100

    Execute prop watch immediately

--- src/components/TasksView.vue
+++ src/components/TasksView.vue
@@ -88,8 +88,11 @@ export default {
   },
   watch: {
     // TODO We maybe need immediate: true (https://stackoverflow.com/a/51176290/9258134), because of the Inbox maybe not loading the tasks when the website is first loaded. Other option: add this.loadData to created hook
-    project: function () {
-      this.loadTasks();
+    project: {
+      immediate: true,
+      handler() {
+        this.loadTasks();
+      }
     }
   },
   methods: {

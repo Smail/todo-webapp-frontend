e9ecbe07e57bf3160ef2f67dd1d38b5b7116ec01
Smail <44433524+Smail@users.noreply.github.com>
2021-11-02 19:23:01 +0100

    feat(content-area): add markdown support
    Add markdown-it.js and the emoji plugin markdown-it-emoji.js

--- src/components/ContentPanel.vue
+++ src/components/ContentPanel.vue
@@ -1,7 +1,23 @@
 <template>
-  <section v-if="activeTask != null">
-    <h1 :data-theme="theme">{{ activeTask.name }}</h1>
-    <textarea :value="activeTask.content"
+  <section v-if="activeTask != null" id="content-section">
+    <div id="header">
+      <h1 :data-theme="theme">{{ activeTask.name }}</h1>
+      <button id="markdown-btn" :data-theme="theme"
+              @click="setMarkdown(!this.displayAsMarkdown)"
+      >Markdown
+      </button>
+    </div>
+    <div v-if="displayAsMarkdown" id="markdown-container"
+         v-html="markdown">
+    </div>
+    <!--    <div v-else contenteditable="true" class="content-area"-->
+    <!--         @focusout="updateServer"-->
+    <!--         @input="setContent($event.target.value)">-->
+    <!--      {{activeTask.content}}-->
+    <!--    </div>-->
+    <textarea v-else id="content-textarea"
+              :value="activeTask.content"
+              class="content-area"
               @focusout="updateServer"
               @input="setContent($event.target.value)">
       {{ activeTask.content }}
@@ -20,9 +36,23 @@ export default {
     return {
       // Will be read by updateServer() function to determine if it should update the server
       didContentChange: false,
+      displayAsMarkdown: true,
+    }
+  },
+  computed: {
+    markdown: function () {
+      return window.markdownit(
+          {
+            "breaks": true,
+          })
+          .use(window.markdownitEmoji)
+          .render(this.activeTask.content);
     }
   },
   methods: {
+    setMarkdown(displayAsMarkdown) {
+      this.displayAsMarkdown = displayAsMarkdown;
+    },
     setContent(newContentStr) {
       this.activeTask.content = newContentStr;
       this.$emit('update:activeTask', this.activeTask);
@@ -55,19 +85,15 @@ export default {
 }
 </script>
 
-<style scoped>
-section {
+<style>
+#content-section {
   width: 100%;
   height: 100%;
   display: flex;
   flex-direction: column;
 }
 
-h1 {
-  padding: 1rem;
-}
-
-textarea {
+#content-textarea {
   flex: 1;
   resize: none;
   outline: none;
@@ -76,5 +102,32 @@ textarea {
   background-color: transparent;
   border: none;
   font-size: 11pt;
+}
+
+#markdown-container * {
+  margin: 1em;
+}
+
+#header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+}
+
+#markdown-btn {
+  color: inherit;
+  font-size: 10pt;
+  width: fit-content;
+  height: fit-content;
+  padding: .75em;
+  background-color: #323232;
+  border: none;
+  border-radius: 2.5px;
+}
+
+#markdown-btn:active {
+  background-color: #5a5a5a;
 }
 </style>

43a97e669b061b4ff73bf4b7a2c74fb5bf080d4f
Smail <44433524+Smail@users.noreply.github.com>
2022-07-08 23:17:07 +0200

    feat(sass): add sass dependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,16 @@
 const webpack = require("webpack");
 
 const webpackConfig = {
+  rules: [
+    {
+      test: /\.scss$/,
+      use: [
+        "vue-style-loader",
+        "css-loader",
+        "sass-loader",
+      ]
+    }
+  ],
   resolve: {
     fallback: {
       buffer: require.resolve("buffer/"),

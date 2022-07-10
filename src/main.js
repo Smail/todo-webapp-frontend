488b2322def4466c1222e8842c4664bdf4a84a9a
Smail <44433524+Smail@users.noreply.github.com>
2022-07-10 18:14:01 +0200

    feat: add light theme

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router"
 import "@/assets/index.scss";
 import "@/assets/themes/theme-dark.scss";
+import "@/assets/themes/theme-light.scss";
 
 window.Buffer = require("buffer/").Buffer;
 

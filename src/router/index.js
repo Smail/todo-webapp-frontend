55576a894aa3c344a3e3e4ef2e9a74d5b63d785c
Smail <44433524+Smail@users.noreply.github.com>
2022-07-11 17:46:48 +0200

    refactor: rename Home.vue => TaskManagerView.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import {createRouter, createWebHistory} from "vue-router"
-import Home from "../views/Home.vue"
+import TaskManager from "../views/TaskManagerView.vue"
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "TaskManager",
+    component: TaskManager
   },
   {
     path: "/calendar",
